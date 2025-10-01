281aaaa9cb3ca7415cfeb01765e5316e864136a9
justaipei <t114015014@ntut.edu.tw>
2025-10-01 15:20:57 +0800

    A click on the button changes the color

---
+++ 4-5_ClickLed/4-5_ClickLed.ino
@@ -0,0 +1,85 @@
+const int buttonPin=2;
+const int rLEDPin=3;
+const int gLEDPin=4;
+const int bLEDPin=5;
+
+
+int buttonState=0; //variable for reading the pushbutton state
+int ledcolor=0;
+
+bool ButtonPressed=false;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  pinMode(rLEDPin, OUTPUT);
+  pinMode(gLEDPin, OUTPUT);
+  pinMode(bLEDPin, OUTPUT);
+  pinMode(buttonPin,INPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  //read the state of the pushbutton value
+  buttonState=digitalRead(buttonPin);
+
+  //check if the pushbutton is pressed (if yes it is LOW)
+  if (buttonState==LOW && !ButtonPressed){
+    //turn led on
+    ledcolor=ledcolor+1;
+    ButtonPressed=true;
+    //delay(100);
+  }
+  if (buttonState==HIGH && ButtonPressed){
+      ButtonPressed=false;
+  }
+  if (ledcolor==0){
+    digitalWrite(rLEDPin, HIGH);
+    digitalWrite(gLEDPin, HIGH);
+    digitalWrite(bLEDPin, HIGH);
+  }
+  else if (ledcolor==1){
+    //red
+    digitalWrite(rLEDPin, LOW);
+    digitalWrite(gLEDPin, HIGH);
+    digitalWrite(bLEDPin, HIGH);
+  }
+  else if (ledcolor==2){
+    //green
+    digitalWrite(rLEDPin, HIGH);
+    digitalWrite(gLEDPin, LOW);
+    digitalWrite(bLEDPin, HIGH);
+  }
+  else if (ledcolor==3){
+    //blue
+    digitalWrite(rLEDPin, HIGH);
+    digitalWrite(gLEDPin, HIGH);
+    digitalWrite(bLEDPin, LOW);
+  }
+  else if (ledcolor==4){
+    //yellow
+    digitalWrite(rLEDPin, LOW);
+    digitalWrite(gLEDPin, LOW);
+    digitalWrite(bLEDPin, HIGH);
+  }
+  else if (ledcolor==5){
+    //purple
+    digitalWrite(rLEDPin, LOW);
+    digitalWrite(gLEDPin, HIGH);
+    digitalWrite(bLEDPin, LOW);
+  }
+  else if (ledcolor==6){
+    //cyan
+    digitalWrite(rLEDPin, HIGH);
+    digitalWrite(gLEDPin, LOW);
+    digitalWrite(bLEDPin, LOW);
+  }
+  else if (ledcolor==7){
+    //white
+    digitalWrite(rLEDPin, LOW);
+    digitalWrite(gLEDPin, LOW);
+    digitalWrite(bLEDPin, LOW);
+  }
+  else if (ledcolor==8){
+    ledcolor=0;
+  }
+}
