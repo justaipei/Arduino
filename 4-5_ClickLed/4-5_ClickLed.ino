1ddd16b3f600a6a5e5eae6dddf19f8ab10b5406f
justaipei <t114015014@ntut.edu.tw>
2025-10-01 15:39:59 +0800

    Clicking the button change the color of the light
    + serial monitor print the color

--- 4-5_ClickLed/4-5_ClickLed.ino
+++ 4-5_ClickLed/4-5_ClickLed.ino
@@ -9,12 +9,16 @@ int ledcolor=0;
 
 bool ButtonPressed=false;
 
+String currentcolor="led";
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   pinMode(rLEDPin, OUTPUT);
   pinMode(gLEDPin, OUTPUT);
   pinMode(bLEDPin, OUTPUT);
   pinMode(buttonPin,INPUT);
+
+  Serial.begin(9600);
 }
 
 // the loop function runs over and over again forever
@@ -22,6 +26,9 @@ void loop() {
   //read the state of the pushbutton value
   buttonState=digitalRead(buttonPin);
 
+  Serial.print("current color: ");
+  Serial.println(currentcolor);
+  
   //check if the pushbutton is pressed (if yes it is LOW)
   if (buttonState==LOW && !ButtonPressed){
     //turn led on
@@ -36,48 +43,56 @@ void loop() {
     digitalWrite(rLEDPin, HIGH);
     digitalWrite(gLEDPin, HIGH);
     digitalWrite(bLEDPin, HIGH);
+    currentcolor="led off";
   }
   else if (ledcolor==1){
     //red
     digitalWrite(rLEDPin, LOW);
     digitalWrite(gLEDPin, HIGH);
     digitalWrite(bLEDPin, HIGH);
+    currentcolor="red";
   }
   else if (ledcolor==2){
     //green
     digitalWrite(rLEDPin, HIGH);
     digitalWrite(gLEDPin, LOW);
     digitalWrite(bLEDPin, HIGH);
+    currentcolor="green";
   }
   else if (ledcolor==3){
     //blue
     digitalWrite(rLEDPin, HIGH);
     digitalWrite(gLEDPin, HIGH);
     digitalWrite(bLEDPin, LOW);
+    currentcolor="blue";
   }
   else if (ledcolor==4){
     //yellow
     digitalWrite(rLEDPin, LOW);
     digitalWrite(gLEDPin, LOW);
     digitalWrite(bLEDPin, HIGH);
+    currentcolor="yellow";
   }
   else if (ledcolor==5){
     //purple
     digitalWrite(rLEDPin, LOW);
     digitalWrite(gLEDPin, HIGH);
     digitalWrite(bLEDPin, LOW);
+    currentcolor="purple";
   }
   else if (ledcolor==6){
     //cyan
     digitalWrite(rLEDPin, HIGH);
     digitalWrite(gLEDPin, LOW);
     digitalWrite(bLEDPin, LOW);
+    currentcolor="cyan";
   }
   else if (ledcolor==7){
     //white
     digitalWrite(rLEDPin, LOW);
     digitalWrite(gLEDPin, LOW);
     digitalWrite(bLEDPin, LOW);
+    currentcolor="white";
   }
   else if (ledcolor==8){
     ledcolor=0;
